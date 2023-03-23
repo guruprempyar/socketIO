838226ee656f345e139a5cdb902fc44d5d171ccc
guruprempyar <guruprem786@gmail.com>
2023-03-23 23:06:15 +0530

    add the alluser on chat page.

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const { registerUser, authUser } = require("../controller/userController");
+const { registerUser, authUser, allUser } = require("../controller/userController");
 
 const router = express.Router();
 
-router.route("/").post(registerUser);
+router.route("/").post(registerUser).get(allUser);
 router.post("/login", authUser);
 
 module.exports = router;
