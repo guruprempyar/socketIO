34e248a0fc2b6c4e3f3ef719e414592569a71e00
guruprempyar <guruprem786@gmail.com>
2023-04-04 19:18:14 +0530

    create the token middleware and user list api.

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,30 @@
+const jwt = require("jsonwebtoken");
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+
+const protect = asyncHandler(async (req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+
+      //decode token id
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      req.user = await User.findById(decoded.id).select("-password");
+      next();
+    } catch (err) {
+      console.error("err", err.message);
+      res.status(401);
+      throw new Error("Invalid token.");
+    }
+  } else {
+    res.status(400);
+    throw new Error("The token is required.");
+  }
+});
+
+module.exports = protect;
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,14 @@
 const express = require("express");
-const { registerUser, authUser, allUser } = require("../controller/userController");
+const {
+  registerUser,
+  authUser,
+  allUser,
+} = require("../controller/userController");
+const protect = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
-router.route("/").post(registerUser).get(allUser);
+router.route("/").post(registerUser).get(protect, allUser);
 router.post("/login", authUser);
 
 module.exports = router;
